8e06857cdc6602c53211a5b95c04388918f038fc
Stephan Plepelits <skunk@xover.mud.at>
2012-11-05 17:36:53 +0100

    Init: define default includes in modulekit.php

--- modulekit.php
+++ modulekit.php
@@ -4,3 +4,10 @@ $name="OpenStreetBrowser - Database";
 $id="openstreetbrowser_database";
 
 $depend=array("hooks");
+
+$default_include=array(
+  "pgsql-functions"=>array("inc/*.sql"),
+  "pgsql-init"=>array("init.sql"),
+  "pgsql-after-import"=>array("after-import.sql"),
+  "php"=>array("inc/*.php"),
+);
